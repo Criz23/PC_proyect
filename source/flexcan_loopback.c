883bb905018f47155fb88e1f9fc512885e286b80
Javier_Home <j.rosales@alumnos.udg.mx>
2023-05-22 18:48:02 -0600

    Clean_code_final

--- source/flexcan_loopback.c
+++ source/flexcan_loopback.c
@@ -6,112 +6,41 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <Can.h>
-//#include "fsl_debug_console.h"
-//#include "fsl_flexcan.h"
+#include "Can.h"
 #include "pin_mux.h"
-//#include "clock_config.h"
 #include "board.h"
 
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define EXAMPLE_CAN            CAN0
-#define EXAMPLE_CAN_CLK_SOURCE (kFLEXCAN_ClkSrc1)
-//#define EXAMPLE_CAN_CLK_FREQ   CLOCK_GetFreq(kCLOCK_BusClk)
-/* Set USE_IMPROVED_TIMING_CONFIG macro to use api to calculates the improved CAN / CAN FD timing values. */
-#define USE_IMPROVED_TIMING_CONFIG (1U)
-#define EXAMPLE_FLEXCAN_IRQn       CAN0_ORed_Message_buffer_IRQn
-#define EXAMPLE_FLEXCAN_IRQHandler CAN0_ORed_Message_buffer_IRQHandler
-#define RX_MESSAGE_BUFFER_NUM      (9)
 #define TX_MESSAGE_BUFFER_NUM      (8)
-
-/* Fix MISRA_C-2012 Rule 17.7. */
-#define LOG_INFO (void)PRINTF
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
 
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 volatile bool rxComplete = false;
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
-/**
-void EXAMPLE_FLEXCAN_IRQHandler(void)
-{
-	uint32_t flag = 1U;
-    // If new data arrived.
-    if (0U != FLEXCAN_GetMbStatusFlags(EXAMPLE_CAN, flag << RX_MESSAGE_BUFFER_NUM))
-    {
-        FLEXCAN_ClearMbStatusFlags(EXAMPLE_CAN, flag << RX_MESSAGE_BUFFER_NUM);
-        (void)FLEXCAN_ReadRxMb(EXAMPLE_CAN, RX_MESSAGE_BUFFER_NUM, &rxFrame);
-        rxComplete = true;
-    }
-    SDK_ISR_EXIT_BARRIER;
-}*/
 
-/*!
- * @brief Main function
- */
 int main(void)
 {
-
-
-    //uint32_t flag = 1U;
-
     /* Initialize board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
 
-    //LOG_INFO("\r\n==FlexCAN loopback functional example -- Start.==\r\n\r\n");
-
-    /*flexcan_timing_config_t timing_config;
-    memset(&timing_config, 0, sizeof(flexcan_timing_config_t));*/
-
-    /*if (FLEXCAN_CalculateImprovedTimingValues(EXAMPLE_CAN, flexcanConfig.baudRate, EXAMPLE_CAN_CLK_FREQ,
-                                              &timing_config))
-    {
-        //Update the improved timing configuration
-        memcpy(&(flexcanConfig.timingConfig), &timing_config, sizeof(flexcan_timing_config_t));
-    }
-    else
-    {
-        LOG_INFO("No found Improved Timing Configuration. Just used default configuration\r\n\r\n");
-    }*/
-
+    // Inicilaizacion de componentes
     CAN_Init(&flexcanConfig);
 
+    // Funcion de escritura
     Can_WriteTxMb(TX_MESSAGE_BUFFER_NUM, &mbConfig);
 
-    /* Enable Rx Message Buffer interrupt. */
-    //FLEXCAN_EnableMbInterrupts(EXAMPLE_CAN, flag << RX_MESSAGE_BUFFER_NUM);
-    //(void)EnableIRQ(EXAMPLE_FLEXCAN_IRQn);
-
-    /*LOG_INFO("Send message from MB%d to MB%d\r\n", TX_MESSAGE_BUFFER_NUM, RX_MESSAGE_BUFFER_NUM);
-    LOG_INFO("tx word0 = 0x%x\r\n", txFrame.dataWord0);
-    LOG_INFO("tx word1 = 0x%x\r\n", txFrame.dataWord1);*/
-
-/* Send data through Tx Message Buffer using polling function. */
+    //Funcion de lectura
     Can_MainFunction_Read();
 
     /* Waiting for Message receive finish. */
     while (!rxComplete)
     {
     }
-
-    /*LOG_INFO("\r\nReceived message from MB%d\r\n", RX_MESSAGE_BUFFER_NUM);
-    LOG_INFO("rx word0 = 0x%x\r\n", rxFrame.dataWord0);
-    LOG_INFO("rx word1 = 0x%x\r\n", rxFrame.dataWord1);*/
-
-    /* Stop FlexCAN Send & Receive. */
-    //FLEXCAN_DisableMbInterrupts(EXAMPLE_CAN, flag << RX_MESSAGE_BUFFER_NUM);
-
-    //LOG_INFO("\r\n==FlexCAN loopback functional example -- Finish.==\r\n");
 
     while (true)
     {
